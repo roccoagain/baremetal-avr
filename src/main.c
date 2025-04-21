1bdd169bcf35eaf68cccade678dcece44c4276b7
Rocco Antoniou <roccoantoniou@gmail.com>
2025-04-21 12:28:04 -0600

    more sep

--- src/main.c
+++ src/main.c
@@ -3,24 +3,6 @@
 #include "motor.h"
 #include "setup.h"
 
-Motor left_motor = {
-    .fwd_port = &PORTE.OUT,
-    .fwd_pin = PIN0_bm,
-    .rev_port = &PORTE.OUT,
-    .rev_pin = PIN1_bm,
-    .pwm_reg = &TCA0.SPLIT.LCMP0, // LCMP0 drives WO0 (PB0)
-};
-
-Motor right_motor = {
-    .fwd_port = &PORTA.OUT,
-    .fwd_pin = PIN1_bm,
-    .rev_port = &PORTE.OUT,
-    .rev_pin = PIN3_bm,
-    .pwm_reg = &TCA0.SPLIT.HCMP1, // HCMP1 drives WO1 (PB1)
-};
-
-
-
 int main(void) {
     setup_pwm();
 
--- src/motor.h
+++ src/motor.h
@@ -3,7 +3,6 @@
 
 #include <avr/io.h>
 
-
 /// @brief Motor configuration struct
 typedef struct {
     /// @brief Port register for forward direction pin
@@ -17,5 +16,21 @@ typedef struct {
     /// @brief PWM register
     volatile uint8_t* pwm_reg;
 } Motor;
+
+Motor left_motor = {
+    .fwd_port = &PORTE.OUT,
+    .fwd_pin = PIN0_bm,
+    .rev_port = &PORTE.OUT,
+    .rev_pin = PIN1_bm,
+    .pwm_reg = &TCA0.SPLIT.LCMP0, // LCMP0 drives WO0 (PB0)
+};
+
+Motor right_motor = {
+    .fwd_port = &PORTA.OUT,
+    .fwd_pin = PIN1_bm,
+    .rev_port = &PORTE.OUT,
+    .rev_pin = PIN3_bm,
+    .pwm_reg = &TCA0.SPLIT.HCMP1, // HCMP1 drives WO1 (PB1)
+};
 
 #endif
